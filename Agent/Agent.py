6c4e81b26ac804700bfae87cdea783ea52e16b4c
cn-54 <238699711+Cn-54@users.noreply.github.com>
2026-09-07 22:03:37 +0100

    Added extra logging and fixed a few bugs

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,9 +1,10 @@
 import os
 import discord
 import requests
+import socket
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = os.getenv("GUILD_ID")
+GUILD_ID = int(os.getenv("GUILD_ID"))
 HOSTNAME = socket.gethostname()
 
 MODULES_CATEGORY_NAME = "MODULES"
@@ -26,23 +27,32 @@ async def setup():
     category = discord.utils.get(guild.categories, name = AGENTS_CATEGORY_NAME) 
 
     if not category: # checks if AGENTS category exists if it doesnt create it
+        print(" [!] AGENTS Category not found")
         category = await guild.create_category(AGENTS_CATEGORY_NAME)
+        print(" [+] AGENTS Category created")
 
     categoryModules = discord.utils.get(guild.categories, name = MODULES_CATEGORY_NAME) 
 
     if not categoryModules: # checks if MODULES category exists if it doesnt create it
-        category = await guild.create_category(MODULES_CATEGORY_NAME)
-    
+        print(" [!] MODULES Category not found")
+        categoryModules = await guild.create_category(MODULES_CATEGORY_NAME)
+        print(" [+] MODULES Category created")
     
     agent_channel = discord.utils.get(guild.text_channels, name=HOSTNAME)
 
     if not agent_channel: # checks if this agents channel exists if not it creates the channel
+        print(" [!] AGENTS Channel not found")
         agent_channel = await guild.create_text_channel( HOSTNAME, category=category)
-    
+        print(" [+] AGENTS Channel created")
+
+
     modules_channel = discord.utils.get( guild.text_channels,name=MODULES_CHANNEL_NAME)
 
     if not modules_channel:
-        agent_channel = await guild.create_text_channel(MODULES_CHANNEL_NAME, category=category)
+        print(" [!] MODULES Channel not found")
+        modules_channel = await guild.create_text_channel(MODULES_CHANNEL_NAME, category=category)
+        print(" [+] MODULES Channel created")
+
 
 
 @bot.event
