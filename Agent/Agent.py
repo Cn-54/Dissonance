0276433aa66bae70249a761f2be5dff3af335e57
cn-54 <238699711+Cn-54@users.noreply.github.com>
2026-09-07 21:53:56 +0100

    started work on agent.py added the bot setup and bot startup

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,11 +1,56 @@
-FROM python:3.12-slim
+import os
+import discord
+import requests
 
-WORKDIR /app
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD_ID = os.getenv("GUILD_ID")
+HOSTNAME = socket.gethostname()
 
-COPY requirements.txt /app/requirements.txt
+MODULES_CATEGORY_NAME = "MODULES"
+MODULES_CHANNEL_NAME = "modules"
+AGENTS_CATEGORY_NAME = "AGENTS"
 
-RUN pip install --no-cache-dir -r /app/requirements.txt
+intents = discord.Intents.default()
+intents.message_content = True
 
-COPY . /app
+bot = discord.Client(intents=intents)
 
-CMD ["python", "main.py"]
+
+async def setup():
+    guild = bot.get_guild(GUILD_ID)
+
+    if not guild: # checks if guild actually exists
+        print(" [!] Guild not found")
+        exit()
+    
+    category = discord.utils.get(guild.categories, name = AGENTS_CATEGORY_NAME) 
+
+    if not category: # checks if AGENTS category exists if it doesnt create it
+        category = await guild.create_category(AGENTS_CATEGORY_NAME)
+
+    categoryModules = discord.utils.get(guild.categories, name = MODULES_CATEGORY_NAME) 
+
+    if not categoryModules: # checks if MODULES category exists if it doesnt create it
+        category = await guild.create_category(MODULES_CATEGORY_NAME)
+    
+    
+    agent_channel = discord.utils.get(guild.text_channels, name=HOSTNAME)
+
+    if not agent_channel: # checks if this agents channel exists if not it creates the channel
+        agent_channel = await guild.create_text_channel( HOSTNAME, category=category)
+    
+    modules_channel = discord.utils.get( guild.text_channels,name=MODULES_CHANNEL_NAME)
+
+    if not modules_channel:
+        agent_channel = await guild.create_text_channel(MODULES_CHANNEL_NAME, category=category)
+
+
+@bot.event
+async def on_ready():
+    print(f"Connected as {bot.user}")
+    await setup()
+
+
+
+
+bot.run(TOKEN)
